6c81c291cfc1b8ec78fbcc3dc62c27adac6bcf87
thenu97 <thenuja_1@outlook.com>
2020-03-19 11:30:40 +0000

    added file

--- tests/testing.py
+++ tests/testing.py
@@ -23,27 +23,27 @@ app.config['SECRET_KEY'] = 'secret'
 
 def test_home():
     http = urllib3.PoolManager()
-    r = http.request('GET', 'http://35.230.137.31:5000/')
+    r = http.request('GET', 'http://localhost:5000/')
     assert 200 == r.status
 
 def test_about():
     http = urllib3.PoolManager()
-    r = http.request('GET', 'http://35.230.137.31:5000/about')
+    r = http.request('GET', 'http://localhost:5000/about')
     assert 200 == r.status
 
 def test_create():
     http = urllib3.PoolManager()
-    r = http.request('GET', 'http://35.230.137.31:5000/create')
+    r = http.request('GET', 'http://localhost:5000/create')
     assert 200 == r.status
 
 def test_addtag():
     http = urllib3.PoolManager()
-    r = http.request('GET', 'http://35.230.137.31:5000/tag')
+    r = http.request('GET', 'http://localhost:5000/tag')
     assert 200 == r.status
 
 def test_view():
     http = urllib3.PoolManager()
-    r = http.request('GET', 'http://35.230.137.31:5000/blog/2')
+    r = http.request('GET', 'http://localhost:5000/blog/2')
     assert 200 == r.status
 
 
