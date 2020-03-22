81b9cd483b969592bea75d95d1bc1624111fcb80
thenu97 <thenuja_1@outlook.com>
2020-03-22 14:26:13 +0000

    mistake

--- script/make_script.sh
+++ script/make_script.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 source /var/lib/jenkins/bashrc
-source /var/lib/jenkins/workspace/Pipeline/venv/bin/activate
 
 /var/lib/jenkins/workspace/Pipeline/venv/bin/coverage run --source=. -m pytest /var/lib/jenkins/workspace/Pipeline/tests/testing.py
 /var/lib/jenkins/workspace/Pipeline/venv/bin/coverage report -m
