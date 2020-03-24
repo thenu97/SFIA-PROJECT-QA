963386fa5c26bd0e188087be4992d44cf97862db
thenu97 <61195906+thenu97@users.noreply.github.com>
2020-03-24 15:41:34 +0000

    jenkins2 test2

--- script/make_script.sh
+++ script/make_script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-source /var/lib/jenkins/workspace/Pipeline/venv/bin/activate
+source /var/lib/jenkins/workspace/pipeline/venv/bin/activate
 source /var/lib/jenkins/bashrc
 
-/var/lib/jenkins/workspace/Pipeline/venv/bin/coverage run --omit 'venv/*' -m pytest tests/testing.py
-/var/lib/jenkins/workspace/Pipeline/venv/bin/coverage report -m
+/var/lib/jenkins/workspace/pipeline/venv/bin/coverage run --omit 'venv/*' -m pytest tests/testing.py
+/var/lib/jenkins/workspace/pipeline/venv/bin/coverage report -m
