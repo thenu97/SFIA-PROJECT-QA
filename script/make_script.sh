13c290a9677d6c86436b388169778f4fad060bf9
thenu97 <61195906+thenu97@users.noreply.github.com>
2020-03-24 15:53:23 +0000

    Update make_script.sh

--- script/make_script.sh
+++ script/make_script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-source /var/lib/jenkins/workspace/pipeline/venv/bin/activate
-source /var/lib/jenkins/bashrc
+source /var/lib/jenkins_home/workspace/pipeline/venv/bin/activate
+source /var/lib/jenkins_home/bashrc
 
-/var/lib/jenkins/workspace/pipeline/venv/bin/coverage run --omit 'venv/*' -m pytest tests/testing.py
-/var/lib/jenkins/workspace/pipeline/venv/bin/coverage report -m
+/var/lib/jenkins_home/workspace/pipeline/venv/bin/coverage run --omit 'venv/*' -m pytest tests/testing.py
+/var/lib/jenkins_home/workspace/pipeline/venv/bin/coverage report -m
